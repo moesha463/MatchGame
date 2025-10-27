aa88e97b5917f008fcda45a6886c045e0b4520cf
moesha463 <smirnovzena463@gmail.com>
2025-10-27 20:07:05 +0300

    Добавьте файлы проекта.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> foodEmoji = new List<string>()
+            {
+                "🍕", "🍕",
+                "🍔", "🍔",
+                "🍟", "🍟",
+                "🌭", "🌭",
+                "🍿", "🍿",
+                "🍗", "🍗",
+                "🎂", "🎂",
+                "🍫", "🍫",
+            };
+
+            Random rnd = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = rnd.Next(foodEmoji.Count);
+                string nextEmoji = foodEmoji[index];  
+                textBlock.Text = nextEmoji;
+                foodEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
